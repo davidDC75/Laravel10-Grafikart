c003886dc6c6e641779d3054a831ea654b84afee
David De Castro <segphault@gmail.com>
2023-09-29 15:49:52 +0200

    Changement style et mod BlogController

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\BlogFilterRequest;
+//use App\Http\Requests\BlogFilterRequest;
 use App\Models\Post;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
+//use Illuminate\Support\Facades\Validator;
+//use Illuminate\Validation\Rule;
 use Illuminate\View\View;
 
 class BlogController extends Controller
 {
-    public function index(BlogFilterRequest $request): View {
+    public function index(): View {
         //dd($request->validated());
         return view('blog.index',[
             'posts' => Post::paginate(10,['id','title','slug'])
@@ -23,6 +23,7 @@ class BlogController extends Controller
         // On récupère le post avec son id
         $post = Post::find($id);
 
+        dd($post);
         // Si il y a une erreur sur le slug dans l'url alors on redirige
         // vers la bonne url
         if ($post->slug !== $slug) {
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -5,8 +5,8 @@
 @section('content')
     <h1>Mon blog</h1>
     @foreach($posts as $post)
-        <article>
-            <h2>{{ $post->title }}</h2>
+        <article class="card">
+            <h5 class="card-title">{{ $post->title }}</h5>
             <a href="{{ route('blog.show', ['slug' => $post->slug, 'id' => $post->id]) }}" class="btn btn-primary">Lire la suite</a>
         </article>
     @endforeach
